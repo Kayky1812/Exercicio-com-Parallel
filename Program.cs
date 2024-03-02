c2a8bfb3b5329b2296b3aead2cd393d4ba32d4ad
Kayky Santos Da Cunha Couto <134162809+Kayky1812@users.noreply.github.com>
2024-03-02 12:35:15 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExemploParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime inicio, fim;
+
+            inicio = DateTime.Now;
+
+            Parallel.Invoke(
+               () =>
+               {
+                   Console.WriteLine("Iniciando Processo1");
+                   Thread.Sleep(5000);
+                   Console.WriteLine("Finalizando Processo1");
+                   Console.WriteLine("Iniciando Processo4");
+                   Thread.Sleep(3000);
+                   Console.WriteLine("Finalizando Processo4");
+               },
+               () =>
+               {
+                   Console.WriteLine("Iniciando Processo2");
+                   Thread.Sleep(4000);
+                   Console.WriteLine("Finalizando Processo2");
+                   Console.WriteLine("Iniciando Processo5");
+                   Thread.Sleep(5000);
+                   Console.WriteLine("Finalizando Processo5");
+               },
+               () =>
+               {
+                   Console.WriteLine("Iniciando Processo3");
+                   Thread.Sleep(3000);
+                   Console.WriteLine("Finalizando Processo3");
+               }
+            );
+            Console.WriteLine("Iniciando Processo6");
+            Thread.Sleep(1000);
+            Console.WriteLine("Finalizando Processo6");
+            fim = DateTime.Now;
+            Console.WriteLine("Tudo encerrado após " + (fim - inicio) + " segundos.");
+            Console.ReadKey();
+        }
+    }
+}
